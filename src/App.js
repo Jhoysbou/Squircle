a7317daab3c842d01ecef0c5d3b074e2a54a050a
jhoysbou <neeroll21@gmail.com>
2020-08-24 04:11:45 +0300

    resize canvas

--- src/App.js
+++ src/App.js
@@ -13,8 +13,13 @@ function App(props) {
         let img = new Image();
         img.src = URL.createObjectURL(imgFile);
         const ctx = canvas.current.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
+
 
         img.onload = () => {
+            canvas.current.width = img.width;
+            canvas.current.height = img.height;
+
             ctx.drawImage(img, 0, 0, img.width, img.height,     // source rectangle
                 0, 0, canvas.current.width, canvas.current.height);
             processImage(ctx);
@@ -35,18 +40,17 @@ function App(props) {
 
         let j = 0;
         for (let i = 0; i < data.length; i += 4) {
-            let x = i / 4  - center.x - j * canvas.current.width;
+            let x = i / 4 - center.x - j * canvas.current.width;
             let y = center.y - j;
 
             if (!isContain(x, y, a, b, n)) {
-                imgData.data[i+3] = 0;
+                imgData.data[i + 3] = 0;
             }
 
             if (i !== 0 && i / 4 % canvas.current.width === 0) {
                 j++;
             }
         }
-
 
         ctx.putImageData(imgData, 0, 0);
     };
@@ -55,11 +59,21 @@ function App(props) {
         return Math.pow(Math.abs(x / a), n) + Math.pow(Math.abs(y / b), n) <= 1;
     };
 
+    const onDownloadClick = () => {
+        let link = document.createElement('a');
+        link.download = 'Squircle.png';
+        link.href = canvas.current.toDataURL()
+        link.click();
+    };
+
 
     return (
         <div className="App">
             <input type={"file"} onChange={onChange}/>
             <canvas ref={canvas} height={size[0]} width={size[0]}/>
+            <button onClick={onDownloadClick}>
+                Download
+            </button>
         </div>
     );
 }
