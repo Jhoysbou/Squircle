95b745a2dcb31528cc80f34f843df59e6017ff4d
jhoysbou <neeroll21@gmail.com>
2020-08-25 04:27:35 +0300

    slider added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, {createRef, useState} from 'react';
+import React, {createRef, useEffect, useState} from 'react';
 import './App.css';
-import {convert} from "./util/convert";
 
 
 function App(props) {
-    const [size, setSize] = useState([150, 150]);
+    const [img, setImg] = useState();
+    const [n, setN] = useState(4);
     const canvas = createRef();
     const canvas1 = createRef();
-    const n = 4;
     const pica = require('pica')();
 
-    const onChange = (e) => {
+    const onFileChange = e => {
         const imgFile = e.target.files[0];
-        let img = new Image();
-        img.src = URL.createObjectURL(imgFile);
-        const ctx = canvas.current.getContext('2d');
-        ctx.imageSmoothingEnabled = true;
-
-
-        img.onload = () => {
-            canvas.current.width = img.width;
-            canvas.current.height = img.height;
-
-            ctx.drawImage(img, 0, 0, img.width, img.height,     // source rectangle
-                0, 0, canvas.current.width, canvas.current.height);
-            processImage(ctx);
-        }
+        let image = new Image();
+        image.src = URL.createObjectURL(imgFile);
+        setImg(image);
     };
 
+    const onSliderChange = e => {
+        setN(e.target.value);
+    };
+
+    useEffect(() => {
+        const ctx = canvas.current.getContext('2d');
+
+        if (img !== undefined) {
+            if (img.complete) {
+                processImage(ctx);
+            } else {
+                img.onload = () => processImage(ctx);
+            }
+
+        }
+
+    });
+
     const processImage = (ctx) => {
+        canvas.current.width = img.width;
+        canvas.current.height = img.height;
+
+        ctx.drawImage(img, 0, 0, img.width, img.height,     // source rectangle
+            0, 0, canvas.current.width, canvas.current.height);
+
         let imgData = ctx.getImageData(0, 0, canvas.current.width, canvas.current.height);
         let data = imgData.data;
 
@@ -54,57 +66,15 @@ function App(props) {
             }
         }
 
-        // data = antialiasing(imgData.data, canvas.current.width);
-        // const AAImgData = new ImageData(data, canvas.current.width, canvas.current.height);
-
         ctx.putImageData(imgData, 0, 0);
+
         pica.resize(canvas.current, canvas1.current, {
             alpha: true,
-            unsharpAmount: 80,
-            unsharpRadius: 0.6,
-            unsharpThreshold: 2
-        })
-            .then(result => console.log('resize done!'));
+            unsharpAmount: 0,
+            unsharpRadius: 0.3,
+            unsharpThreshold: 0
+        });
     };
-
-    const antialiasing = (pixels, width) => {
-        let img = convert(pixels, width);
-        const result = [];
-        for (let i = 0; i < width; i++) {
-
-            let length = img[i].length;
-            for (let j = 0; j < length; j++) {
-
-                let red = 0;
-                let green = 0;
-                let blue = 0;
-                let alpha = 0;
-
-                let counter = 0;
-
-                for (let k = -1; k < 2; k++) {
-                    for (let l = -1; l < 2; l++) {
-                        try {
-                            red += img[i + k][j + l].getR();
-                            green += img[i + k][j + l].getG();
-                            blue += img[i + k][j + l].getB();
-                            alpha += img[i + k][j + l].getA();
-                            counter++;
-                        } catch (e) {
-                        }
-                    }
-                }
-
-                result.push(red / counter);
-                result.push(green / counter);
-                result.push(blue / counter);
-                result.push(alpha / counter);
-            }
-        }
-
-        return Uint8ClampedArray.from(result);
-    }
-
 
     const isContain = (x, y, a, b, n) => {
         return Math.pow(Math.abs(x / a), n) + Math.pow(Math.abs(y / b), n) <= 1;
@@ -120,12 +90,22 @@ function App(props) {
 
     return (
         <div className="App">
-            <input type={"file"} accept="image/*" onChange={onChange}/>
-            <canvas ref={canvas} height={size[0]} width={size[0]}/>
+            <input type={"file"}
+                   accept="image/*"
+                   onChange={onFileChange}/>
+            <input type={"range"}
+                   min={0}
+                   max={10}
+                   onChange={onSliderChange}
+                   step={0.1}/>
+            <canvas style={{display: "none"}}
+                    ref={canvas}
+                    height={500}
+                    width={500}/>
             <button onClick={onDownloadClick}>
                 Download
             </button>
-            <canvas ref={canvas1} height={150} width={150} />
+            <canvas ref={canvas1} height={150} width={150}/>
         </div>
     );
 }
